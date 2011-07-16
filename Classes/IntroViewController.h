df3d72fcf146e31d7a402254c08e650c8163a84e
Vladimir Fleurima <vladimir.fleurima@gmail.com>
2011-07-16 10:39:44 -0700

    1st commit

---
+++ Classes/IntroViewController.h
@@ -0,0 +1,18 @@
+//
+//  IntroViewController.h
+//  Radar
+//
+//  Created by Vladimir Fleurima (iBook) on 7/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface IntroViewController : UIViewController {
+	IBOutlet UIButton *facebookButton;
+}
+
+@property (nonatomic, retain) IBOutlet UIButton *facebookButton;
+- (IBAction)facebookSignin;
+@end
---
+++ Classes/RadarAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  RadarAppDelegate.h
+//  Radar
+//
+//  Created by Vladimir Fleurima (iBook) on 7/16/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RadarAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
